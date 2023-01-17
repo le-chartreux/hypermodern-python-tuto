9113e3bd6e2373c9a14ec8592379b457d67c2be6
le-chartreux <le-chartreux-vert@protonmail.com>
2023-01-18 00:40:06 +0100

    printings in the UI module

--- src/hypermodern_python/__main__.py
+++ src/hypermodern_python/__main__.py
@@ -1,20 +1,22 @@
-import textwrap
-
 import click
 
 import hypermodern_python
+import hypermodern_python.ui
 import hypermodern_python.wikipedia.requester
 
 
 @click.command()
 @click.version_option(version=hypermodern_python.__version__)
 @click.option("--french/--english", is_flag=True, default=False,  help="Set the language of the page")
-def main(french: bool):
-    """The hypermodern Python project."""
+def main(french: bool) -> None:
+    """
+    Displays the title and the summary of a random Wikipedia article
+
+    :param french: whether the language of the Wikipedia page should be French, else English
+    """
     wikipedia_requester = hypermodern_python.wikipedia.requester.WikipediaRequester()
     if french:
         wikipedia_requester.set_language_to_french()
     wikipedia_article = wikipedia_requester.get_random_article()
 
-    click.secho(wikipedia_article.title, fg="green")
-    click.echo(textwrap.fill(wikipedia_article.summary))
+    hypermodern_python.ui.print_wikipedia_article(wikipedia_article)
---
+++ src/hypermodern_python/ui.py
@@ -0,0 +1,10 @@
+import textwrap
+
+import click
+
+import hypermodern_python.wikipedia.article
+
+
+def print_wikipedia_article(wikipedia_article: hypermodern_python.wikipedia.article.WikipediaArticle) -> None:
+    click.secho(wikipedia_article.title, fg="green")
+    click.echo(textwrap.fill(wikipedia_article.summary))
