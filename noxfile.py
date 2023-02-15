2332019cdc70a7c42d998e412196e528f91be504
le-chartreux <le-chartreux-vert@protonmail.com>
2023-02-15 15:43:28 +0100

    adding formatting with blake

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,10 @@
 import nox
 
+nox.options.sessions = "lint", "tests"
+
 code_locations = "src", "test", "noxfile.py"
 python_versions = "3.10", "3.11"
+latest_python = python_versions[-1]
 
 
 @nox.session(python=python_versions)
@@ -13,10 +16,20 @@ def tests(session: nox.sessions.Session) -> None:
     session.run(runner, "run", "pytest", *args, external=True)
 
 
-@nox.session(python=python_versions)
+@nox.session(python=latest_python)
 def lint(session: nox.sessions.Session) -> None:
     """Runs linting"""
     linter = "flake8"
+    linter_plugins = "flake8-black"
     args = session.posargs or code_locations
-    session.install(linter)
+    session.install(linter, linter_plugins)
     session.run(linter, *args)
+
+
+@nox.session(python=latest_python)
+def reformat(session: nox.sessions.Session) -> None:
+    """Runs code formatting"""
+    formatter = "black"
+    args = session.posargs or code_locations
+    session.install(formatter)
+    session.run(formatter, *args)
