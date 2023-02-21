352586eda3b8297fc07f2221a896ff73901a7472
le-chartreux <le-chartreux-vert@protonmail.com>
2023-02-21 19:26:16 +0100

    fields in WikipediaArticleSchema all now required

--- src/hypermodern_python/wikipedia/article_schema.py
+++ src/hypermodern_python/wikipedia/article_schema.py
@@ -4,12 +4,12 @@ from hypermodern_python.wikipedia.article import WikipediaArticle
 
 
 class WikipediaArticleSchema(marshmallow.Schema):
-    title = marshmallow.fields.String()
-    extract = marshmallow.fields.String(attribute="summary")
+    title = marshmallow.fields.String(required=True)
+    extract = marshmallow.fields.String(required=True, attribute="summary")
 
     @marshmallow.post_load
     def make_article(self, data, **_kwargs) -> WikipediaArticle:
         return WikipediaArticle(**data)
 
-    class Meta:
+    class Meta(marshmallow.schema.SchemaMeta):
         unknown = marshmallow.EXCLUDE
