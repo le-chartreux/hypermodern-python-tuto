ed1751185e953cc273fc36218e36e9e6692ef271
le-chartreux <le-chartreux-vert@protonmail.com>
2023-02-26 18:01:50 +0100

    making _check_article_correct protected

--- test/wikipedia/test_article_schema.py
+++ test/wikipedia/test_article_schema.py
@@ -12,16 +12,17 @@ def test_init() -> None:
 def test_load_simple() -> None:
     schema = WikipediaArticleSchema()
     article = schema.load({"title": title, "extract": extract})
-    check_article_correct(article)
+    _check_article_correct(article)
 
 
 def test_load_with_unknown() -> None:
     schema = WikipediaArticleSchema()
     article = schema.load({"title": title, "extract": extract, "something": "unwanted"})
-    check_article_correct(article)
+    _check_article_correct(article)
 
 
-def check_article_correct(article: WikipediaArticle) -> None:
+# utils part
+def _check_article_correct(article: WikipediaArticle) -> None:
     assert isinstance(article, WikipediaArticle)
     assert article.title == title
     assert article.summary == extract
