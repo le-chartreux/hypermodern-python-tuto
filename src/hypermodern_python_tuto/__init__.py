b8b3eaea01290a3080b690243d2fbca8708d292e
le-chartreux <le-chartreux-vert@protonmail.com>
2023-03-17 17:19:17 +0100

    [style] removing an extra blank line

--- src/hypermodern_python_tuto/__init__.py
+++ src/hypermodern_python_tuto/__init__.py
@@ -1,7 +1,6 @@
 """The hypermodern Python project."""
 import importlib.metadata
 
-
 try:
     __version__ = importlib.metadata.version(__name__)
 except importlib.metadata.PackageNotFoundError:  # pragma: no cover
