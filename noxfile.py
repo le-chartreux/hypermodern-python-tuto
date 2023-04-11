6593e3869516d7a57f4e1afd7b9ad8b37ed3edd8
Nathan Rousseau <59126928+le-chartreux@users.noreply.github.com>
2023-04-11 18:23:15 +0200

    [ci] better structure of noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,20 +3,21 @@ import tempfile
 
 import nox
 
+# options
 nox.options.sessions = "pytest", "doctest", "ruff", "safety", "mypy"
 nox.options.reuse_existing_virtualenvs = True
-silent_default = True
-silent_format = False
+SILENT_DEFAULT = True
+SILENT_CODE_MODIFIERS = False
+RUNNER = "poetry"
 
-package_location = "./src/hypermodern_python_tuto"
-code_locations = package_location, "./test", "./noxfile.py"
-python_versions = "3.10", "3.11"
-latest_python = python_versions[-1]
+# targets
+PACKAGE_LOCATION = "./src/hypermodern_python_tuto"
+CODE_LOCATIONS = PACKAGE_LOCATION, "./test", "./noxfile.py"
+PYTHON_VERSIONS = "3.10", "3.11"
+LATEST_PYTHON = PYTHON_VERSIONS[-1]
 
-runner = "poetry"
 
-
-@nox.session(python=python_versions, tags=["test"])
+@nox.session(python=PYTHON_VERSIONS, tags=["test"])
 def pytest(session: nox.Session) -> None:
     """Run the test suite with pytest."""
     args = session.posargs or ("--cov", "-m", "not e2e")
@@ -24,44 +25,44 @@ def pytest(session: nox.Session) -> None:
     _run(session, "pytest", *args)
 
 
-@nox.session(python=python_versions, tags=["test"])
+@nox.session(python=PYTHON_VERSIONS, tags=["test"])
 def doctest(session: nox.Session) -> None:
     """Run doctests with pytest."""
-    session.notify(f"pytest-{session.python}", (package_location, "--doctest-modules"))
+    session.notify(f"pytest-{session.python}", (PACKAGE_LOCATION, "--doctest-modules"))
 
 
-@nox.session(python=latest_python, tags=["lint"])
+@nox.session(python=LATEST_PYTHON, tags=["lint"])
 def ruff(session: nox.Session) -> None:
     """Lint with Ruff."""
-    args = session.posargs or code_locations
+    args = session.posargs or CODE_LOCATIONS
     _install(session)
     _run(session, "ruff", "check", *args)
 
 
-@nox.session(python=latest_python, tags=["format"])
+@nox.session(python=LATEST_PYTHON, tags=["format"])
 def black(session: nox.Session) -> None:
     """Reformat with black."""
-    args = session.posargs or code_locations
+    args = session.posargs or CODE_LOCATIONS
     _install(session)
-    _run(session, "black", *args, silent=silent_format)
+    _run_code_modifier(session, "black", *args)
 
 
-@nox.session(python=latest_python, tags=["format"])
+@nox.session(python=LATEST_PYTHON, tags=["format"])
 def isort(session: nox.Session) -> None:
     """Reformat the import order with isort."""
-    args = session.posargs or code_locations
+    args = session.posargs or CODE_LOCATIONS
     _install(session)
-    _run(session, "isort", *args, silent=silent_format)
+    _run_code_modifier(session, "isort", *args)
 
 
-@nox.session(python=latest_python, tags=["security"])
+@nox.session(python=LATEST_PYTHON, tags=["security"])
 def safety(session: nox.Session) -> None:
     """Scan dependencies for insecure packages."""
     # not within poetry because it conflicts with black
     # (and since its safety there is no reason to not use the latest)
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
-            runner,
+            RUNNER,
             "export",
             "--with",
             "dev",
@@ -69,7 +70,7 @@ def safety(session: nox.Session) -> None:
             "--without-hashes",
             f"--output={requirements.name}",
             external=True,
-            silent=silent_default,
+            silent=SILENT_DEFAULT,
         )
         session.install("safety")
         session.run(
@@ -77,26 +78,26 @@ def safety(session: nox.Session) -> None:
             "check",
             f"--file={requirements.name}",
             "--full-report",
-            silent=silent_default,
+            silent=SILENT_DEFAULT,
         )
 
 
-@nox.session(python=python_versions, tags=["typecheck"])
+@nox.session(python=PYTHON_VERSIONS, tags=["typecheck"])
 def mypy(session: nox.Session) -> None:
     """Verify types using mypy (so it is static)."""
-    args = session.posargs or code_locations
+    args = session.posargs or CODE_LOCATIONS
     _install(session)
     _run(session, "mypy", *args)
 
 
-@nox.session(python=latest_python, tags=["documentation"])
+@nox.session(python=LATEST_PYTHON, tags=["documentation"])
 def docs(session: nox.Session) -> None:
     """Build the documentation."""
     _install(session)
     _run(session, "sphinx-build", "docs", "docs/_build")
 
 
-@nox.session(python=latest_python, tags=["ci"])
+@nox.session(python=LATEST_PYTHON, tags=["ci"])
 def coverage(session: nox.Session) -> None:
     """Upload coverage data."""
     args = session.posargs or [
@@ -112,10 +113,14 @@ def coverage(session: nox.Session) -> None:
 
 # everything after this line is utils
 def _install(session: nox.Session, *args: str) -> None:
-    session.run(runner, "install", *args, external=True, silent=silent_default)
+    session.run(RUNNER, "install", *args, external=True, silent=SILENT_DEFAULT)
 
 
 def _run(
-    session: nox.Session, target: str, *args: str, silent: bool = silent_default
+    session: nox.Session, target: str, *args: str, silent: bool = SILENT_DEFAULT
 ) -> None:
-    session.run(runner, "run", target, *args, external=True, silent=silent)
+    session.run(RUNNER, "run", target, *args, external=True, silent=silent)
+
+
+def _run_code_modifier(session: nox.Session, target: str, *args: str) -> None:
+    _run(session, target, *args, silent=SILENT_CODE_MODIFIERS)
